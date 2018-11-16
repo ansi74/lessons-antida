1aaf08190cdeec1e59287c0a870e17a11d5b3ff6
ansi <ansi@chelcom.ru>
2018-11-16 19:49:31 +0500

    added timer

--- task-2/app.js
+++ task-2/app.js
@@ -1,21 +1,27 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question('Введите название файла...(без расширения):', (answer) => {
-    var fs = require("fs");
-    var fileName = answer;
-    fs.readFile(fileName + ".txt", "utf8",
-            function (error, data) {
-                if (error)
-                    throw 'Ошибка. Файл не существует или не считывается';
-                fs.writeFile("converted_" + fileName + ".txt", data.replace(/\s{2,}/g, ' '), function(error){
-					if(error) throw 'Ошибка записи'; 
-					console.log("Запись файла завершена. Содержимое файла:");
-					var data = fs.readFileSync("converted_" + fileName + ".txt", "utf8");
-					console.log(data);
+function removeSpace (){
+	const readline = require('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	rl.question('Введите название файла...(без расширения):', (answer) => {
+		var fs = require("fs");
+		var fileName = answer;
+		var TIMER;
+		console.time(TIMER);
+		fs.readFile(fileName + ".txt", "utf8",
+				function (error, data) {
+					if (error)
+						throw 'Ошибка. Файл не существует или не считывается';
+					fs.writeFile("converted_" + fileName + ".txt", data.replace(/\s{2,}/g, ' '), function(error){
+						if(error) throw 'Ошибка записи'; 
+						console.log("Запись файла завершена. Содержимое файла:");
+						var data = fs.readFileSync("converted_" + fileName + ".txt", "utf8");
+						console.log(data);
+					});
 				});
-            });
-    rl.close();
-});
+		rl.close();
+		console.timeEnd(TIMER);
+	});
+}
+ removeSpace ()
